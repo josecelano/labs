e8b8c70400cb8b4d9249b35aa6036eb48f70208d
Celano, Jose <jose.celano@hyve.net>
2017-02-28 11:44:46 +0000

    3-lifecycles-performance

--- src/Plot.js
+++ src/Plot.js
@@ -8,5 +8,55 @@
  * To see an example of how you should use Plotly look at the index.html file in the public/ folder.
  * (and feel free to delete the code in there)
  */
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Plotly from 'plotly.js/dist/plotly.js';
+
+class Plot extends Component {
+    componentDidMount() {
+        this.renderGraph();
+    }
+
+    componentDidUpdate() {
+        this.renderGraph();
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if (nextProps.xData !== this.props.xData) {
+            return true;
+        }
+        if (nextProps.yData !== this.props.yData) {
+            return true;
+        }
+        if (nextProps.type !== this.props.type) {
+            return true;
+        }
+        return false;
+    }
+
+    renderGraph = () => {
+        Plotly.newPlot(this.graph, [
+            {
+                x: this.props.xData,
+                y: this.props.yData,
+                type: this.props.type
+            }
+        ], {
+            margin: {
+                t: 0,
+                r: 0,
+                l: 30
+            },
+            xaxis: {
+                gridcolor: 'transparent'
+            }
+        }, {displayModeBar: false});
+    }
+
+    render() {
+        return (<div ref={(graph) => {
+            this.graph = graph;
+        }}/>);
+    }
+}
+
+export default Plot;

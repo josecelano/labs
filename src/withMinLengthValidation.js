7ed88d7923720c3ad5c3a3164ded0e22bb24219b
Celano, Jose <jose.celano@hyve.net>
2017-03-02 10:45:19 +0000

    7-3-hoc

--- src/withMinLengthValidation.js
+++ src/withMinLengthValidation.js
@@ -1,1 +1,10 @@
 import React from 'react';
+
+const withMinLengthValidation = (Component, minLength) => {
+  const warning = <span>Minimum {minLength} required.</span>;
+  return (props) => {
+    return <div><Component {...props} />{ props.value.length < minLength ? warning : ''}</div>
+  }
+}
+
+export default withMinLengthValidation
